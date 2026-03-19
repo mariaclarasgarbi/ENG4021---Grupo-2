ecb3e9d9507b96766460b4037cd187335ca2dc58
Yuri de Sousa Dias <diasyuri752@gmail.com>
2026-03-19 13:37:19 +0000

    .

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -5,16 +5,16 @@ import divisao
 
 aux=int(input("Informe a operação desejada:\n1-Soma\n2-Subtracao\n3-Multiplicacao\n4-Divisao\n"))
 if(aux==1):
-    resultado=int(soma.soma())
+    resultado=int(soma.somaf())
     print(f"Resultado: {resultado}")
 elif(aux==2):
-    resultado=int(subtracao.subtracao())
+    resultado=int(subtracao.subtracaof())
     print(f"Resultado: {resultado}")
 elif(aux==3):
-    resultado=int(multiplicacao.multiplicacao())
+    resultado=int(multiplicacao.multiplicacaof())
     print(f"Resultado: {resultado}")
 elif(aux==4):
-    resultado=int(divisao.divisao())
+    resultado=int(divisao.divisaof())
     print(f"Resultado: {resultado}")
 
 
